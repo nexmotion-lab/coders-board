01048abae01d9bfb2436ae281da572f39ab5a2cc
choivember <pocketmongyo@gmail.com>
2024-01-16 14:36:55 +0900

    free crud 구현

--- src/main/java/com/nexmotion/board/free/FreeController.java
+++ src/main/java/com/nexmotion/board/free/FreeController.java
@@ -1,4 +1,15 @@
 package com.nexmotion.board.free;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
 public class FreeController {
+
+    @RequestMapping("/free")
+    public String noticePage() {
+
+        return "/free/free";
+    }
+
 }
--- src/main/java/com/nexmotion/board/free/FreeMapper.java
+++ src/main/java/com/nexmotion/board/free/FreeMapper.java
@@ -1,4 +1,17 @@
 package com.nexmotion.board.free;
 
+import com.nexmotion.board.notice.Notice;
+import org.apache.ibatis.annotations.Mapper;
+
+import java.sql.SQLException;
+import java.util.List;
+
+@Mapper
 public interface FreeMapper {
+
+    public List<Free> selectFree(Free free) throws SQLException;
+    public void insertFree(Free free) throws SQLException;
+    public void updateFree(Free free) throws SQLException;
+    public void deleteFree(Free free) throws SQLException;
+
 }
