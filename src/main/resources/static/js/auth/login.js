b2c1b2de68b159ee4ad15a1753298b7dc159439b
sejin0211 <sejin0211@naver.com>
2024-01-22 16:45:27 +0900

    [editing] enter 키로 로그인하기 구현

--- src/main/resources/static/js/auth/login.js
+++ src/main/resources/static/js/auth/login.js
@@ -1,12 +1,14 @@
 let login = {}
 
 login.fn = {
-
+    // 초기화
     init: function() {
         login.fn.ready();
     },
 
     ready : function() {
+
+        // 로그인 버튼 클릭
         $(document).on('click', '#login', function() {
 
             let id = $('#username');
@@ -26,6 +28,30 @@ login.fn = {
 
             $('#frm').submit();
         });
+
+        // 엔터키 입력
+        $(document).on('keydown', function (event) {
+            let key = event.which || event.keyCode;
+            if (key == 13) {
+                let id = $('#username');
+                let pwd = $('#password');
+
+                if (id.val() === "") {
+                    alert('사용자 아이디를 입력하여 주십시오');
+                    id.focus();
+                    return;
+                }
+
+                if (pwd.val() === "") {
+                    alert('비밀번호를 입력하여 주십시오');
+                    pwd.focus();
+                    return;
+                }
+
+                $('#frm').submit();
+                return;
+            }
+        })
     }
 };
 
