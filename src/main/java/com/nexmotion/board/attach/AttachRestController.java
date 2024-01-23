d4b263eef591965777c30890e83430a8a7a96cd2
sejin0211 <sejin0211@naver.com>
2024-01-23 12:35:25 +0900

    [adding] 자료실 글 작성 cdEditor 구현

--- src/main/java/com/nexmotion/board/attach/AttachRestController.java
+++ src/main/java/com/nexmotion/board/attach/AttachRestController.java
@@ -7,6 +7,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.time.LocalDateTime;
@@ -20,6 +21,7 @@ public class AttachRestController {
     AttachService attachService;
 
     @RequestMapping("/attach/select")
+    @ResponseBody
     public ResponseObject<List<Attach>> select (
             @RequestParam(value = "postId", required = false) Integer postId,
             @RequestParam(value = "postAuthor", required = false) String postAuthor,
