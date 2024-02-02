6dad13364cc2fbfb2f44f0c5c23a7255a150906a
DongHyeon Yoon <128972134+donghyeon0619@users.noreply.github.com>
2024-02-02 20:30:18 +0900

    formUtils.js 코드 수정

--- src/main/resources/static/js/common/formUtils.js
+++ src/main/resources/static/js/common/formUtils.js
@@ -6,28 +6,25 @@ function checkWriteInput(maxLength){
 
     if(postTitle.trim() === ""){
         alert('제목을 입력하여 주십시오.');
-        setFocus('#postTitle');
+        $('#postTitle').focus();
         return false;
     }
 
     if(encoder.encode(postTitle).length > maxLength){
-        alert('제한된 길이를 넘었습니다. 제목을 다시 입력하여 주십시오.');
-        setFocus('#postTitle');
+        alert('길이 제한을 초과했습니다. 제목을 다시 입력해 주십시오.');
+        $('#postTitle').focus();
         return false;
     }
 
+    // CKeditor 초기화의 비동기 특성에 대해 완전한 초기화 이후에 포커스가 실행되도록 설정
     if(postContent.trim() === ""){
-        alert('길이 제한을 초과했습니다. 제목을 다시 입력해 주십시오.');
-        setFocus('#postContent');
+        alert('내용을 입력해 주십시오');
+        setTimeout(()=>{
+            editor.editing.view.focus();
+        },0);
         return false;
     }
     return true;
 
 }
 
-// CKeditor 초기화의 비동기 특성에 대해 완전한 초기화 이후에 포커스가 실행되도록 설정
-function setFocus(selector){
-    setTimeout(()=>{
-        $(selector).focus();
-    }, 0);
-}
