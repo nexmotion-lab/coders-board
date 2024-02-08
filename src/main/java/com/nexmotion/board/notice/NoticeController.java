9ec61da5a76a0b58aca4093cc8b81061e2a4d88f
choivember <pocketmongyo@gmail.com>
2024-02-08 11:00:46 +0900

    공지사항 CRUD 완성

--- src/main/java/com/nexmotion/board/notice/NoticeController.java
+++ src/main/java/com/nexmotion/board/notice/NoticeController.java
@@ -1,15 +1,21 @@
 package com.nexmotion.board.notice;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class NoticeController {
 
     @RequestMapping("/notice")
-    public String noticePage() {
-
+    public String attachPage(){
         return "/notice/notice";
     }
+    @RequestMapping("/noticeWrite")
+    public String attachWritePage(){ return "/notice/noticeWrite"; }
+    @RequestMapping("/notice/details/{postId}")
+    public String attachDetailsPage(@PathVariable String postId){ return "/notice/noticeDetails"; }
 
+    @RequestMapping("/noticeModify/{postId}")
+    public String attachModifyPage(@PathVariable String postId){return "/notice/noticeModify";}
 }
--- src/main/java/com/nexmotion/board/notice/NoticeMapper.java
+++ src/main/java/com/nexmotion/board/notice/NoticeMapper.java
@@ -1,5 +1,6 @@
 package com.nexmotion.board.notice;
 
+import com.nexmotion.board.attach.Attach;
 import org.apache.ibatis.annotations.Mapper;
 
 import java.sql.SQLException;
@@ -9,8 +10,10 @@ import java.util.List;
 public interface NoticeMapper {
 
     public List<Notice> selectNotice(Notice notice) throws SQLException;
+    public Notice selectNoticeDetails(Notice notice) throws SQLException;
     public void insertNotice(Notice notice) throws SQLException;
     public void updateNotice(Notice notice) throws SQLException;
     public void deleteNotice(Notice notice) throws SQLException;
+    public void updatePostHit(Notice notice) throws  SQLException;
 
 }
